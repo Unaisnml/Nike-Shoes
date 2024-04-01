4ce7e71b2511612c37f68cce1014a6a45abec4dd
Unaisnml <unaisnml@gmail.com>
2024-04-01 10:03:39 +0530

    footer completed

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,4 +1,6 @@
+import { copyrightSign } from "../assets/icons";
 import { footerLogo } from "../assets/images";
+import { footerLinks, socialMedia } from "../constants";
 
 const Footer = () => {
   return (
@@ -8,11 +10,48 @@ const Footer = () => {
           <a href="/">
             <img src={footerLogo} alt="" width={150} height={46} />
           </a>
-          <p>
+          <p className="mt-6 text-base leading-7 font-montserrat sm:max-w-sm text-white-400">
             Thank you for visiting our website! At XYZ Company, we're dedicated
             to providing top-notch products and excellent customer service.
           </p>
+          <div className="mt-8 flex items-center gap-5">
+            {socialMedia.map((icon) => (
+              <div
+                key={icon.alt}
+                className="flex justify-center items-center w-12 h-12 bg-white rounded-full"
+              >
+                <img src={icon.src} alt={icon.alt} width={24} height={24} />
+              </div>
+            ))}
+          </div>
         </div>
+        <div className="flex flex-1 justify-between lg:gap-10 gap-24 flex-wrap">
+          {footerLinks.map((section) => (
+            <div key={section}>
+              <h4 className="text-white font-montserrat text-2xl font-medium leading-normal mb-6">
+                {section.title}
+              </h4>
+              <ul>
+                {section.links.map((link) => (
+                  <li
+                    className="mt-3 text-white-400 font-montserrat text-base hover:text-slate-gray cursor-pointer"
+                    key={link.name}
+                  >
+                    <a>{link.name}</a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="flex justify-between text-white-400 mt-24 max-sm:flex-col max-sm:items-center">
+        <div className="flex flex-1 justify-start items-center gap-2 font-montserrat cursor-pointer">
+          <img src={copyrightSign} alt="copy right sign" width={20} height={20} className="rounded-full m-0 "/>
+          <p >Copyright. All rights reserved.</p>
+        </div>
+        <p className="font-montserrat cursor-pointer">Term & Conditions</p>
+
       </div>
     </footer>
   );
